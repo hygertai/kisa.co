2ffb41e29225949093c0e367eb640dd31379d79f
Gerta <hygerta.imeri@hotmail.com>
2020-04-15 08:53:27 +0300

    removed bottomNavBar in direct short link generation screen

--- kisa_co/lib/screens/directShort_screen.dart
+++ kisa_co/lib/screens/directShort_screen.dart
@@ -92,53 +92,6 @@ class _DirectShortScreenState extends State<DirectShortScreen> {
           ),
         ),
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: 0,
-        fixedColor: kLightPurpleColor,
-        items: [
-          BottomNavigationBarItem(
-            title: Text("Home"),
-            icon: Icon(Icons.home),
-          ),
-          BottomNavigationBarItem(
-            title: Text("Analytics"),
-            icon: Icon(Icons.show_chart),
-          ),
-          BottomNavigationBarItem(
-            title: Text("Dashboard"),
-            icon: Icon(Icons.dashboard),
-          ),
-        ],
-        onTap: (int index) {
-          setState(() {
-            switch (index) {
-              case 0:
-                {
-                  // Navigate to Dashboard
-                  Navigator.pushNamed(context, WelcomeScreen.id);
-                }
-                break;
-              case 1:
-                {
-                  // Navigate to Archived List
-                  Navigator.pushNamed(context, AnalyticsScreen.id);
-                }
-                break;
-              case 2:
-                {
-                  // Map
-                  Navigator.pushNamed(context, DashboardScreen.id);
-                }
-                break;
-              default:
-                {
-                  Navigator.pushNamed(context, WelcomeScreen.id);
-                }
-                break;
-            }
-          });
-        },
-      ),
     );
   }
 }
